9cefc9d5c1d4e6bae0343a5b844d3c517ddfde1e
J0skaa <szeplaki.jozsef97@gmail.com>
2025-01-23 16:40:10 +0100

    Created a check_disk_usage script, which outputs the disk usage stats in a human-readable format.

--- scripts/run_all_checks.sh
+++ scripts/run_all_checks.sh
@@ -11,5 +11,13 @@ echo
 echo "2. Running check_file_permissions.sh..."
 ./check_file_permissions.sh
 
+echo 
+echo "3. Run monitor_logs.sh..."
+./monitor_logs.sh
+
+echo
+echo "4. Run check_disk_usage.sh..."
+./check_disk_usage.sh
+
 echo
 echo "All checks completed."
